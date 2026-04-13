04e206536a15b992e28eade87d174798ead14523
wjdah1028 <wjdah1028@naver.com>
2026-04-13 10:39:29 +0900

    :tada:Start:프로젝트 생성

---
+++ src/main/java/com/capd/capdbackend/CapdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capd.capdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capd/capdbackend/CapdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capd.capdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
