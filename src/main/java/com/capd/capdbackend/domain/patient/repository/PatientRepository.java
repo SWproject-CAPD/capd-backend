0c211239fc9e23e450c6bea935c3dbd1acc101ad
wjdah1028 <wjdah1028@naver.com>
2026-04-17 13:51:30 +0900

    :recycle: Refactor: user 테이블에서 사용자 찾는 메서드 추가

--- src/main/java/com/capd/capdbackend/domain/patient/repository/PatientRepository.java
+++ src/main/java/com/capd/capdbackend/domain/patient/repository/PatientRepository.java
@@ -1,7 +1,13 @@
 package com.capd.capdbackend.domain.patient.repository;
 
 import com.capd.capdbackend.domain.patient.entity.PatientEntity;
+import com.capd.capdbackend.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<PatientEntity, Long> {
+
+    // user 도메인에 있는 사용자와 patient 도메인에 있는 환자가 같은 사람인지 확인
+    Optional<PatientEntity> findByUser(UserEntity user);
 }
