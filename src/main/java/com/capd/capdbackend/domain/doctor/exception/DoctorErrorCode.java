9ffafe2ad3faa64a3c2318018b814e546eb0a1ec
wjdah1028 <wjdah1028@naver.com>
2026-04-20 16:18:37 +0900

    :recycle: Refactor: 에러상황 관련 코드 추가

--- src/main/java/com/capd/capdbackend/domain/doctor/exception/DoctorErrorCode.java
+++ src/main/java/com/capd/capdbackend/domain/doctor/exception/DoctorErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum DoctorErrorCode implements BaseErrorCode {
 
     LICENSE_ID_NOT_FOUND("D4001", HttpStatus.NOT_FOUND, "존재하지 않는 면허번호입니다."),
-    LICENSE_ID_DUPLICATE("D4002", HttpStatus.CONFLICT, "중복된 면허번호입니다.");
+    LICENSE_ID_DUPLICATE("D4002", HttpStatus.CONFLICT, "중복된 면허번호입니다."),
+    PATIENT_ALREADY_REGISTERED("D4003", HttpStatus.CONFLICT, "이미 등록된 환자입니다.");
 
 
     private final String code;
