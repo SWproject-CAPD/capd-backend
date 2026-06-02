8df9126e3e6e9c54655809c63e7adbe4c65e9ced
wjdah1028 <wjdah1028@naver.com>
2026-06-02 09:17:17 +0900

    :recycle: Refactor: User 관련 에러코드 추가

--- src/main/java/com/capd/capdbackend/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/capd/capdbackend/domain/user/exception/UserErrorCode.java
@@ -14,7 +14,10 @@ public enum UserErrorCode implements BaseErrorCode {
     DUPLICATE_NICKNAME("U4003", HttpStatus.CONFLICT, "중복된 닉네임입니다."),
     DUPLICATE_PHONE("U4004", HttpStatus.CONFLICT, "중복된 전화번호입니다."),
     PASSWORD_NOT_MATCH("U4005", HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
-    USER_UNAUTHORIZED("U4006", HttpStatus.UNAUTHORIZED, "회원 정보가 일치하지 않습니다.");
+    USER_UNAUTHORIZED("U4006", HttpStatus.UNAUTHORIZED, "회원 정보가 일치하지 않습니다."),
+    VERIFICATION_CODE_EXPIRED("U4007", HttpStatus.BAD_REQUEST, "인증 코드가 만료됐습니다."),
+    INVALID_VERIFICATION_CODE("U4008", HttpStatus.BAD_REQUEST, "유효하지 않은 인증 코드입니다."),
+    EMAIL_NOT_VERIFIED("U4009", HttpStatus.BAD_REQUEST, "이메일 인증이 필요합니다.");
 
     private final String code;
     private final HttpStatus status;
